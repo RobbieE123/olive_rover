e4196bfb0f1b44a3e832a8181a1202023587dc3b
RobbieE123 <158078160+RobbieE123@users.noreply.github.com>
2024-02-26 18:52:01 +0000

    Changed package name

--- launch/gps.launch.py
+++ launch/gps.launch.py
@@ -5,7 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     # Assuming you have a config directory within your package with necessary configuration files
-    config_dir = os.path.join(get_package_share_directory('articubot_one'), 'config')
+    config_dir = os.path.join(get_package_share_directory('olive_rover'), 'config')
 
     return LaunchDescription([
         # GPS Publisher Node
